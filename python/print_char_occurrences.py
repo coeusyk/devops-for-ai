f751a431cdc8e96eac61b0515eceab4821ba15b4
coeusyk <69001824+coeusyk@users.noreply.github.com>
2024-10-21 14:59:39 +0530

    Updated print_char_occurrences.py to work based on command-line arguments for the build pipeline

--- python/print_char_occurrences.py
+++ python/print_char_occurrences.py
@@ -1,8 +1,15 @@
-name = input("Enter Name: ")
+import sys
 
-used_chars = ""
 
-for char in name:
-    if char not in used_chars:
-        print(f"{char}: {name.count(char)}")
-        used_chars += char
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("invalid number of arguments: expected 1")
+
+    else:
+        name = sys.argv[1]
+        used_chars = ""
+
+        for char in name:
+            if char not in used_chars:
+                print(f"{char}: {name.count(char)}")
+                used_chars += char
